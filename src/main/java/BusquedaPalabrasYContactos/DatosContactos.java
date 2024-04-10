9838ada691cfbb70f53b8d9ae6ccd860ff96a84a
fferrlop <fferrlop@myuax.com>
2024-04-10 16:23:18 +0200

    esqueleto contactos

---
+++ src/main/java/BusquedaPalabrasYContactos/DatosContactos.java
@@ -0,0 +1,38 @@
+package BusquedaPalabrasYContactos;
+
+public class DatosContactos {
+    private String nombre;
+    private String email;
+    private String numeroTelefono;
+
+    public DatosContactos(String nombre, String email, String numeroTelefono) {
+        this.nombre = nombre;
+        this.email = email;
+        this.numeroTelefono = numeroTelefono;
+    }
+
+    // Getters and setters
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNumeroTelefono() {
+        return numeroTelefono;
+    }
+
+    public void setNumeroTelefono(String numeroTelefono) {
+        this.numeroTelefono = numeroTelefono;
+    }
+}
